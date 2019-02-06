d91921a2a772ef71d92ff84ad6576ef41211868d
Scott <tabor.scott10@gmail.com>
2019-02-06 12:47:04 -0500

    added a blank Album component and routed it.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         <main>
          <Route exact path="/" component={Landing} />
          <Route path="/library" component={Library} />
+         <Route path="/Album" component={Album} />
         </main>
 
 
---
+++ src/components/Album.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Album = () => (
+  <section className='album'>
+    An album goes here!
+  </section>
+);
+
+
+export default Album; 
